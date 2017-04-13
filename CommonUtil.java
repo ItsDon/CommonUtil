774983368cb5c6ac28375b73be1292e42f489c2c
Itsdon <a1554786707@163.com>
2017-04-13 20:10:35 +0800

    添加获取手机尺寸的方法

--- CommonUtil.java
+++ CommonUtil.java
@@ -4,6 +4,31 @@ package com.rrceo.android.utils;
  * Created by itsdon on 17/4/13.
  */
 
+import android.content.Context;
+
+/**
+ *  常用工具类
+ */
 public class CommonUtil {
+
+
+    /**
+     *  获得手机屏幕宽度
+     * @param context
+     * @return
+     */
+    public static int  getScreenWidth(Context context){
+        return context.getResources().getDisplayMetrics().widthPixels;
+    }
+
+    /**
+     * 获得手机屏幕高度
+     * @param context
+     * @return
+     */
+    public static int getScreenHeight(Context context){
+        return context.getResources().getDisplayMetrics().heightPixels;
+    }
+
 
 }
